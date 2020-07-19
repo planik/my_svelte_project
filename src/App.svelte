78dbad6383033b7372d6889871d566d97dce7c6c
chrigu <christian.muehlethaler@optor.ch>
2020-07-19 19:22:38 +0200

    https://svelte.dev/tutorial/reactive-assignments

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,20 @@
 <script>
 	export let name;
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<button on:click={handleClick}>
+		Clicked {count} {count === 1 ? 'time' : 'times'}
+	</button>
 </main>
 
 <style>
